8fcd3c97e2a51f85afd386260e633b6ccc6e10a1
sainath <sainath90@gmail.com>
2018-12-24 12:11:45 -0500

    INC 348

--- app/src/routes/Orders/Orders.js
+++ app/src/routes/Orders/Orders.js
@@ -608,7 +608,7 @@ export default class OrderView extends PureComponent {
                 key: 'IND',
                 render: (text, record) => {
 
-                    let value = text === 'A'? "Not Batched":text === 'B'? "Partially Batched":text === 'C'? "Completely Batched":text === 'D'? "Not relavent for shipping":text === 'E'?"Partially shipped":text === 'F'?"Shipped Completely":text === 'G'?"Not Relavant for Invoice":text === 'H'?"Partially invoiced":text === 'I'?"Fully Invoiced":text === 'J'?"Credit Hold":text === 'K'?"Customer Service Hold":text === 'L'?"Inventory Hold":"Pending";
+                    let value = text === 'A'? "Pending":text === 'B'? "Batched":text === 'C'? "Batched":text === 'D'? "Not relavent for shipping":text === 'E'?"Shipped":text === 'F'?"Shipped":text === 'G'?"Not Relavant for Invoice":text === 'H'?"Invoiced":text === 'I'?"Invoiced":text === 'J'?"Pending":text === 'K'?"Pending":text === 'L'?"Pending":"Pending";
                   
                    
                     return (
